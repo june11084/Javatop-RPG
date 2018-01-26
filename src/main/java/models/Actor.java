338129aefa3180dab3a2b013365b829d7f66fc39
Jun Li <june11084@gmail.com>
2018-01-26 13:57:01 -0800

    have actor methods

--- src/main/java/models/Actor.java
+++ src/main/java/models/Actor.java
@@ -10,9 +10,45 @@ public abstract class Actor {
 
   public Actor(String name, int hitPoints, int strength) {
     this.name = name;
-    this.hitPoints = hitPoints; // the health of the Actor
-    this.strength = strength;   // this Actor throws a strength-sided die
+    this.hitPoints = hitPoints;
+    this.strength = strength;
 
     die = new Random();
+  }
+
+  public String toString() {
+    return name + "  hit points: " + hitPoints + " strength: " + strength;
+  }
+
+  public void strike(Actor opponent) {
+    System.out.println(name + " strikes at " + opponent.getName());
+
+    int ourThrow = throwDie();
+    int oppThrow = opponent.throwDie();
+
+    if (ourThrow > oppThrow) {
+      System.out.println("Good Hit!!");
+      System.out.println(opponent.getName() + " loses a hit point");
+      opponent.takeHit(1);
+    } else {
+      System.out.println(opponent.getName() + " ducks and is not hurt.");
+    }
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void takeHit(int points) {
+    hitPoints -= points;
+  }
+
+  public int getHitpoints() {
+    return hitPoints;
+  }
+
+  protected int throwDie() {
+    return die.nextInt(strength) + 1;
   }
 }
