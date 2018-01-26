b5984d89c492d42d4ca2bdca4356e764ecdad1a2
Jun Li <june11084@gmail.com>
2018-01-26 13:45:24 -0800

    have Actor.java class outline

--- src/main/java/models/Actor.java
+++ src/main/java/models/Actor.java
@@ -1,4 +1,18 @@
 package models;
 
-public class Actor {
+import java.util.Random;
+
+public abstract class Actor {
+  protected int hitPoints;
+  protected int strength;
+  protected String name;
+  private Random die;
+
+  public Actor(String name, int hitPoints, int strength) {
+    this.name = name;
+    this.hitPoints = hitPoints; // the health of the Actor
+    this.strength = strength;   // this Actor throws a strength-sided die
+
+    die = new Random();
+  }
 }
